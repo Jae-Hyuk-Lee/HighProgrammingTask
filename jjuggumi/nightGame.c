b5d542f8cc4d91cb5e4f46c9912ea2dd9440167b
YunHyeok <104815459+yunhyeok374@users.noreply.github.com>
2023-12-01 19:24:29 +0900

    야간운동 포함

---
+++ jjuggumi/nightGame.c
@@ -0,0 +1,41 @@
+#include "nightGame.h"
+
+void nightgame() {
+
+    system("cls");
+
+    nightGameInit();
+
+    int tick = 0;
+
+    while (1) {
+
+        tick += 1;
+
+        printNightGameMap();
+
+        key_t key = get_key();
+        if (key == K_QUIT) {
+            break;
+        }
+        else if (key != K_UNDEFINED) {
+            movePlayer(key);
+        }
+
+        if (tick >= 5) {
+            for (int i = 1; i < n_player; i++) {
+                moveAI(i);
+            }
+            tick = 0;
+        }
+
+        for (int i = 0; i < n_player; i++) {
+            crushItem(playerPos[i][0], playerPos[i][1], i);
+            crushPlayer(playerPos[i][0], playerPos[i][1], i);
+        }
+
+        Sleep(1000 / 20);
+
+    }
+
+}
